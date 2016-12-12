d0fa07d29cb38fe283a75399702fb660ae81d250
chirag1992m <chirag1992m.cc@gmail.com>
2016-12-12 16:38:04 -0500

    Benchmark (Majority Class) model added. Generated the submission file for this model

---
+++ benchmark.lua
@@ -0,0 +1,28 @@
+--[[
+The purpose of this file is to create a benchmark submission file with majority class model
+
+label 0 - 25810
+label 1 - 2443
+label 2 - 5292
+label 3 - 873
+label 4 - 708
+]]--
+
+-- Accuracy of the majority model: 0.73478335 (According to training data)
+
+local dir = io.popen("ls ./data/test_data/")
+
+lines = {}
+for line in dir:lines() do
+    lines[#lines + 1] = line
+end
+
+local submission = assert(io.open("./submissions/submission_benchmark.csv", "w"))
+submission:write("image,level\n")
+
+for i=1, #lines do
+    filename, file_extension = lines[i]:match("([^.]+).([^,]+)")
+    submission:write(filename .. ',0\n')	--Writing 0 label for every class
+end
+
+submission:close()
